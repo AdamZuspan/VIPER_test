2d65d3864a7aea9cc7dd63ceb0edeca5d9bd3332
Dad <ZuS@adams-macbook-pro.local>
2018-11-01 20:26:45 -0400

    Initial Commit

---
+++ VIPER_Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIPER_Practice
+//
+//  Created by Adam Lee Zuspan on 11/1/18.
+//  Copyright © 2018 Adam Lee Zuspan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
